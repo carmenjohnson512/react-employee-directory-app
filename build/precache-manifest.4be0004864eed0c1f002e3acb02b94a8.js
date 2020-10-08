bee2ef9a5621b2d5bcaaf28eaef6033fd64f6a2b
Carmen Johnson <carmenjohnson512@gmail.com>
2020-10-07 23:11:10 -0500

    prepping for heroku deployment

---
+++ build/precache-manifest.4be0004864eed0c1f002e3acb02b94a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2707fc9139ed0ed0c4b92478e99359",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "112f7271f3704bbcf1cc",
+    "url": "/react-employee-directory/static/css/main.f189dfee.chunk.css"
+  },
+  {
+    "revision": "8eceff7316fa84d3a4f6",
+    "url": "/react-employee-directory/static/js/2.feb4fcad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-employee-directory/static/js/2.feb4fcad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112f7271f3704bbcf1cc",
+    "url": "/react-employee-directory/static/js/main.ab7e5487.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
